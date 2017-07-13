4196b470d2b053f583cf200aeb160d07c9fc540e
Yuri Miliutin <SBT-Milyutin-YUA@mail.ca.sbrf.ru>
2017-07-13 15:27:07 +0300

    The scratch of a PersonGenerator

--- src/test/java/com/example/demo/JsonGeneratorApplicationTests.java
+++ src/test/java/com/example/demo/JsonGeneratorApplicationTests.java
@@ -1,6 +1,9 @@
 package com.example.demo;
 
+import com.example.converter.JacksonMapper;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import org.junit.Test;
+import static org.assertj.core.api.Assertions.*;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -9,12 +12,16 @@ import org.springframework.test.context.junit4.SpringRunner;
 public class JsonGeneratorApplicationTests {
 
 	@Test
-	public void generatePerson() {
+	public void generatePerson() throws JsonProcessingException {
 
 		PersonGenerator personGenerator = new PersonGenerator();
 		Person generate = personGenerator.generate(10000, 100);
 
 		System.out.println(generate);
+
+		JacksonMapper jacksonMapper = new JacksonMapper();
+		String json = jacksonMapper.toJson(generate);
+		System.out.println(json);
 	}
 
 }
